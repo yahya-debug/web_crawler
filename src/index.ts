234fb69087c8468d8ae34a05988375f018cbdde4
yahya-debug <68112561+yahya-debug@users.noreply.github.com>
2026-08-24 20:13:19 +0300

    now app takes 1 argument on start

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, web crawler is here");
+import { argv } from "node:process";
+
+function main() {
+    if (argv.length != 3)
+        throw new Error("app takes just one argument");
+
+    const baseURL = argv[2];
+
+    console.log(`started crawling ${argv[2]}`)
+    process.exit(0);
+}
+
+try {
+    main();
+} catch (e) {
+    console.error((e as Error).message);
+    process.exit(1);
+}
